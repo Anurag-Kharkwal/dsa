caae49b75808fa202aee36f142b25610de29e24a
Anurag-Kharkwal <94605412+Anurag-Kharkwal@users.noreply.github.com>
2022-01-19 17:01:18 +0000

    Add files via upload

---
+++ leftmostrep.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int leftmost(string s)
+{
+	int i,count[256]={0};
+	for(i=0;i<s.length();i++)
+	count[s[i]]++;
+	for(i=0;i<s.length();i++)
+	{
+	if(count[s[i]]>1)
+	return i;
+    }
+    return -1;
+}
+int main()
+{
+	string s="kfaccbb";
+	cout<<leftmost(s);
+}
